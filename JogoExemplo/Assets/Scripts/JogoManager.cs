fe6ab31acd80c39fa6e215c50feea858d66f2ff7
marxistpokemon <enric.llagostera@gmail.com>
2014-04-12 18:46:54 -0300

    fluxo de telas do jogo

---
+++ JogoExemplo/Assets/Scripts/JogoManager.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Tela {
+	TITULO,
+	INGAME,
+	GAMEOVER
+}
+
+// essa classe eh um singleton
+public class JogoManager : MonoBehaviour {
+
+	public Tela telaAtual;
+	public bool gameOver;
+
+	#region Singleton
+	private static JogoManager _instance;
+
+	public static JogoManager i
+	{
+		get
+		{
+			if(_instance == null)
+			{
+				_instance = GameObject.FindObjectOfType<JogoManager>();
+				
+				//Tell unity not to destroy this object when loading a new scene!
+				DontDestroyOnLoad(_instance.gameObject);
+			}
+			
+			return _instance;
+		}
+	}
+	
+	void Awake() 
+	{
+		if(_instance == null)
+		{
+			//If I am the first instance, make me the Singleton
+			_instance = this;
+			DontDestroyOnLoad(this);
+		}
+		else
+		{
+			//If a Singleton already exists and you find
+			//another reference in scene, destroy it!
+			if(this != _instance)
+				Destroy(this.gameObject);
+		}
+	}
+	#endregion
+
+	public void Update()
+	{
+		// Avanca as telas - FIXME corrigir o input
+		if(Input.GetButton("Jump")){
+			TrocaTela();
+		}
+	}
+
+	public void TrocaTela()
+	{
+		switch (telaAtual) {
+		case Tela.TITULO : 
+			telaAtual = Tela.INGAME;
+			Application.LoadLevel("Ingame");
+			break;
+		case Tela.INGAME : 
+			if(gameOver){
+				telaAtual = Tela.GAMEOVER;
+				Application.LoadLevel("GameOver");
+				gameOver = false;
+			}
+			else {
+				telaAtual = Tela.TITULO;
+				Application.LoadLevel("Titulo");
+			}
+			break;
+		case Tela.GAMEOVER : 
+			telaAtual = Tela.TITULO;
+			Application.LoadLevel("Titulo");
+			break;
+		default:
+			break;
+		}
+	}
+}
