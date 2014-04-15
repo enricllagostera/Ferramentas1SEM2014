9b1a80168a3bc6dd367a8c9a6b25f979d6c205e7
marxistpokemon <enric.llagostera@gmail.com>
2014-04-15 11:59:15 -0300

    tela inicial pronta -fix #21
    as instrucoes tao meio feias

---
+++ JogoExemplo/Assets/Titulo.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Titulo : MonoBehaviour {
+
+	void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.Return)){
+			GetComponent<Animator>().SetTrigger("ComecarJogo");
+		}
+	}
+
+	void ComecarJogo(){
+		JogoManager.i.TrocaTela();
+	}
+}
