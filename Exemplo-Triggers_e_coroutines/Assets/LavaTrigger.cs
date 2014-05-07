bfa09c083fe0cdb88205b1f512479489c262266c
marxistpokemon <enric.llagostera@gmail.com>
2014-05-07 18:36:19 -0300

    Exemplo dos trigger melhorado

--- Exemplo-Triggers_e_coroutines/Assets/LavaTrigger.cs
+++ Exemplo-Triggers_e_coroutines/Assets/LavaTrigger.cs
@@ -3,9 +3,16 @@ using System.Collections;
 
 public class LavaTrigger : MonoBehaviour {
 
+	public int cubosDestruidos = 0;
+
+	void Update() {
+		Physics.gravity = new Vector3(0, -10 + (cubosDestruidos*0.2f), 0);
+	}
+
 	// assim que um objeto com collider tocar esse trigger
 	void OnTriggerEnter (Collider outro) {
 		// pega o objeto do collider e o destroi
 		Destroy(outro.gameObject);
+		cubosDestruidos++;
 	}
 }
