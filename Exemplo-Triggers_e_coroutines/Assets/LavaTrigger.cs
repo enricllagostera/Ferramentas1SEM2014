c874b3e2c94f4753b39166eead242a9d7a881fb9
marxistpokemon <enric.llagostera@gmail.com>
2014-05-07 18:26:53 -0300

    Exemplo de coroutine e triggers

---
+++ Exemplo-Triggers_e_coroutines/Assets/LavaTrigger.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LavaTrigger : MonoBehaviour {
+
+	// assim que um objeto com collider tocar esse trigger
+	void OnTriggerEnter (Collider outro) {
+		// pega o objeto do collider e o destroi
+		Destroy(outro.gameObject);
+	}
+}
---
+++ Exemplo-Triggers_e_coroutines/Assets/SpawnCubos.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnCubos : MonoBehaviour {
+
+	public float intervalo;
+	public Transform prefabCubo;
+	
+	void Start () {
+		StartCoroutine(SoltaCubo()); // comeca a corotina
+	}
+	
+	IEnumerator SoltaCubo(){
+		// vai funcionar enquanto o objeto estiver ativo
+		while (gameObject.activeSelf){
+			// cria um novo cubo
+			Transform temp = Instantiate(prefabCubo, transform.position, Quaternion.identity) as Transform;
+			temp.parent = transform;
+			// espera INTERVALO antes de continuar o fluxo
+			yield return new WaitForSeconds(intervalo);
+		}
+	}
+}
