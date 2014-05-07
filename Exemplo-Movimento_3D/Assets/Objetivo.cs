8638a4408830cf725b202acbef929340c8edf7ed
marxistpokemon <enric.llagostera@gmail.com>
2014-05-07 19:17:39 -0300

    exemplo de movimento 3D

---
+++ Exemplo-Movimento_3D/Assets/Objetivo.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Objetivo : MonoBehaviour {
+
+	// quando algum objeto estiver tocando o trigger
+	void OnTriggerStay (Collider outro){
+		// se o objeto tiver a tag player
+		if(outro.CompareTag("Player")){
+			// muda a cor de fundo da camera
+			Camera.main.backgroundColor = Color.Lerp(Camera.main.backgroundColor, Color.white, 0.3f);
+		}
+	}
+}
---
+++ Exemplo-Movimento_3D/Assets/PlataformaMovel.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlataformaMovel : MonoBehaviour {
+
+	public Transform[] ancoras;
+	public int proximaAncora;
+	public float suavizacao;
+
+	// Use this for initialization
+	void Start () {
+		transform.position = ancoras[0].position;
+		proximaAncora = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// interpola a posicao do objeto
+		transform.position = Vector3.Lerp(transform.position, ancoras[proximaAncora].position, 
+		                                  suavizacao * Time.deltaTime);
+		// se chegou bem perto da proxima ancora
+		if(Vector3.Distance(transform.position, ancoras[proximaAncora].position) < 0.2f) {
+			Debug.Log(true);
+			proximaAncora++; // muda para proxima posicao no vetor de ancoras
+			if(proximaAncora >= ancoras.Length) proximaAncora = 0; // se passou da maior posicao, volta a primeira
+		}
+	}
+}
