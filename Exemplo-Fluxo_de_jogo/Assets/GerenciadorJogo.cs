ca460a75c9d2490c935cae4f8458fed7bd8eeb6f
marxistpokemon <enric.llagostera@gmail.com>
2014-05-07 18:06:33 -0300

    Exemplo de fluxo de jogo terminado

---
+++ Exemplo-Fluxo_de_jogo/Assets/GerenciadorJogo.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// enumerador para variavel de controle
+public enum Telas {
+	Primeira,
+	Segunda
+}
+
+/// <summary>
+/// Esse componente controla as principais variaveis 
+/// globais do jogo, como tela atual, score, etc. Essa classe eh um Singleton,
+/// ou seja, so pode existir uma instancia dela em cada cena.
+/// </summary>
+public class GerenciadorJogo : MonoBehaviour {
+
+	public static GerenciadorJogo instancia;
+	public Telas telaAtual;
+
+	// na primeira inicializacao do componente
+	void Awake() {
+		// garante que apenas uma instancia desse componente exista
+		if(GerenciadorJogo.instancia != null) {
+			Destroy(gameObject);
+		}
+		else {
+			instancia = this; // guarda uma referencia a essa instancia na classe estatica
+			DontDestroyOnLoad(transform.gameObject); // evita que esse objeto seja destruido quando troca de cena
+		}
+	}
+
+	// a cada frame do jogo
+	void Update(){
+		// se o jogador pressionar a tecla de espaco
+		if (Input.GetKeyDown(KeyCode.Space)){
+			// dependendo de qual eh a tela atual, carrega uma Cena diferente
+			switch (telaAtual) {
+			case Telas.Primeira : 
+				telaAtual = Telas.Segunda; // muda a variavel de controle
+				Application.LoadLevel("SegundaCena"); // carrega proxima tela
+				break;
+			case Telas.Segunda : 
+				telaAtual = Telas.Primeira;
+				Application.LoadLevel("PrimeiraCena");
+				break;
+			}
+		}
+	}
+}
