8b32985c230f6826ebc96a75515187e7eb845ffc
marxistpokemon <enric.llagostera@gmail.com>
2014-05-14 21:00:52 -0300

    novo exemplo - workflow 2D e 3D

---
+++ Exemplo-Workflow/Assets/Cavaleiro.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// modelo usado : http://opengameart.org/content/animated-knight
+
+public class Cavaleiro : MonoBehaviour {
+	Animator animator;
+	Vector3 dirInput;
+	public float velocidade = 1;
+
+	void Start () {
+		animator = GetComponentInChildren<Animator>();
+	}
+	
+
+	void Update () {
+		dirInput = new Vector3(-Input.GetAxis("Horizontal"), 0, -Input.GetAxis("Vertical"));
+		if(dirInput != Vector3.zero){
+			animator.SetBool("Andando", true);
+		}
+		else {
+			animator.SetBool("Andando", false);
+		}
+		transform.forward = dirInput;
+	}
+
+	void FixedUpdate () {
+		if(dirInput != Vector3.zero)
+			rigidbody.velocity = transform.forward * velocidade;
+	}
+}
---
+++ Exemplo-Workflow/Assets/Personagem2D.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Personagem2D : MonoBehaviour {
+	Animator animator;
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetAxis("Horizontal") != 0){
+			animator.SetBool("Andando", true);
+		}
+		else {
+			animator.SetBool("Andando", false);
+		}
+
+		if(Input.GetAxis("Horizontal") < 0) {
+			transform.localScale = new Vector3(-1,1,1);
+		}
+		else {
+			transform.localScale = new Vector3(1,1,1);
+		}
+	}
+}
